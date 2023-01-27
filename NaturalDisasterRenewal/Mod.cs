d9032cbe49886b0e406a7ef87ee420381824be64
luisgobo <lagbtech@hotmail.com>
2023-01-26 22:23:22 -0600

    New integration on .NetFrmewok 4.8 created

---
+++ NaturalDisasterRenewal/Mod.cs
@@ -0,0 +1,17 @@
+﻿
+
+using ICities;
+
+namespace NaturalDisasterRenewal
+{
+    public class NaturalDisasterRenewalMod : IUserMod
+    {
+        public string Name => "Natural Disaster Renewal";
+
+        public string Description => "Natural disaster conyinuation for Natural Disaster overhaul and some Ragnarok's mod stuffs";
+
+        public NaturalDisasterRenewalMod() { 
+        
+        }   
+    }
+}
