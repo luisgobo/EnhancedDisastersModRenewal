86460788bd74d136f258fd901849c78f8e85979f
luisgobo <lagbtech@hotmail.com>
2023-02-17 21:27:02 -0600

    Fix for Sinkhole destruction Radius

--- Source/Services/LegacyStructure/NaturalDisaster/SinkholeService.cs
+++ Source/Services/LegacyStructure/NaturalDisaster/SinkholeService.cs
@@ -3,7 +3,9 @@ using ColossalFramework.IO;
 using ICities;
 using NaturalDisastersRenewal.Common;
 using NaturalDisastersRenewal.Common.enums;
+using NaturalDisastersRenewal.Models;
 using NaturalDisastersRenewal.Services.LegacyStructure.Handlers;
+using System;
 
 namespace NaturalDisastersRenewal.Services.LegacyStructure.NaturalDisaster
 {
@@ -64,19 +66,6 @@ namespace NaturalDisastersRenewal.Services.LegacyStructure.NaturalDisaster
             return base.GetProbabilityTooltip();
         }
 
-        public override void OnDisasterStarted(byte intensity)
-        {
-            groundwaterAmount = 0;
-
-            base.OnDisasterStarted(intensity);
-        }
-
-        //public override void OnDisasterDetected(DisasterInfoModel disasterInfoUnified)
-        //{
-        //    disasterInfoUnified.DisasterInfo.type = DisasterType.Sinkhole;
-        //    base.OnDisasterDetected(disasterInfoUnified);
-        //}
-
         protected override void OnSimulationFrameLocal()
         {
             float daysPerFrame = Helper.DaysPerFrame;
@@ -95,6 +84,35 @@ namespace NaturalDisastersRenewal.Services.LegacyStructure.NaturalDisaster
             }
         }
 
+        public override void OnDisasterActivated(DisasterSettings disasterInfo, ushort disasterId)
+        {
+            disasterInfo.type |= DisasterType.Sinkhole;
+            base.OnDisasterActivated(disasterInfo, disasterId);
+        }
+
+        public override void OnDisasterDeactivated(DisasterInfoModel disasterInfoUnified)
+        {
+            disasterInfoUnified.DisasterInfo.type |= DisasterType.Sinkhole;
+            disasterInfoUnified.EvacuationMode = EvacuationMode;
+            disasterInfoUnified.IgnoreDestructionZone = false;
+            base.OnDisasterDeactivated(disasterInfoUnified);
+        }
+
+        public override void OnDisasterDetected(DisasterInfoModel disasterInfoUnified)
+        {
+            disasterInfoUnified.DisasterInfo.type |= DisasterType.Sinkhole;
+            disasterInfoUnified.EvacuationMode = EvacuationMode;
+            disasterInfoUnified.IgnoreDestructionZone = false;
+
+            base.OnDisasterDetected(disasterInfoUnified);
+        }
+
+        public override void OnDisasterStarted(byte intensity)
+        {
+            groundwaterAmount = 0;
+            base.OnDisasterStarted(intensity);
+        }
+
         protected override float GetCurrentOccurrencePerYearLocal()
         {
             return base.GetCurrentOccurrencePerYearLocal() * groundwaterAmount / GroundwaterCapacity;
@@ -108,6 +126,48 @@ namespace NaturalDisastersRenewal.Services.LegacyStructure.NaturalDisaster
         public override string GetName()
         {
             return "Sinkhole";
+        }
+
+        public override float CalculateDestructionRadio(byte intensity)
+        {
+            int unitSize = 8;
+            int unitsBase = 24; //24 + 4 Original, Distance Fix for proximity
+            float unitCalculation;
+            int intensityInt = intensity / 10;
+            int intensityDec = intensity % 10;
+
+            switch (intensity)
+            {
+                case byte n when (n < 26):
+                    unitCalculation = ((((intensityInt - 5) * 10) + intensityDec) * 0.4f) + unitsBase - (0.28f * intensityDec) - (intensityInt * 2.8f);
+                    break;
+                case byte n when (n >= 26 && n < 101):
+                    unitCalculation = ((((intensityInt - 5) * 10) + intensityDec) * 0.4f) + unitsBase - (0.28f * intensityDec) - (intensityInt * 2.8f) - ((intensityDec - 5) * 0.04f) - ((intensityInt - 2) * 0.4f);
+                    break;
+                case byte n when (n >= 101 && n < 126):
+                    unitCalculation = ((((intensityInt - 5) * 10) + intensityDec) * 0.4f) + unitsBase - (0.28f * intensityDec) - (intensityInt * 2.8f) - 3f;
+                    break;
+                case byte n when (n >= 126 && n <151):
+                    unitCalculation = ((((intensityInt - 5) * 10) + intensityDec) * 0.4f) + unitsBase - (0.28f * intensityDec) - (intensityInt * 2.8f) - ((intensityDec - 5) * 0.04f) - ((intensityInt - 2) * 0.3f) - ((intensityDec - 5) * 0.04f) - (0.5f * (intensityInt - 12));
+                    break;
+                case byte n when (n >= 151 && n < 176):
+                    unitCalculation = ((((intensityInt - 5) * 10) + intensityDec) * 0.4f) + unitsBase - (0.28f * intensityDec) - (intensityInt * 2.8f) - ((intensityDec - 5) * 0.04f) - ((intensityInt - 2) * 0.4f);
+                    break;
+                case byte n when (n >= 176 && n < 201):
+                    unitCalculation = ((((intensityInt - 5) * 10) + intensityDec) * 0.4f) + unitsBase - (0.28f * intensityDec) - (intensityInt * 2.8f) - ((intensityDec - 5) * 0.04f) - ((intensityInt - 2) * 0.4f) + ((intensityDec - 5) * 0.08f) + ((intensityInt - 17) * 0.8f);
+                    break;
+                case byte n when (n >= 201 && n < 226):
+                    unitCalculation = ((((intensityInt - 5) * 10) + intensityDec) * 0.4f) + unitsBase - (0.28f * intensityDec) - (intensityInt * 2.8f) - ((intensityDec - 5) * 0.04f) - ((intensityInt - 2) * 0.3f) - ((intensityDec - 5) * 0.04f) - (0.5f * (intensityInt - 12)) + 4f;
+                    break;
+                case byte n when (n >= 226 && n < 251):
+                    unitCalculation = ((((intensityInt - 5) * 10) + intensityDec) * 0.4f) + unitsBase - (0.28f * intensityDec) - (intensityInt * 2.8f) - ((intensityDec - 5) * 0.04f) - ((intensityInt - 2) * 0.4f) + 1;
+                    break;
+                default:
+                    unitCalculation = ((((intensityInt - 5) * 10) + intensityDec) * 0.4f) + unitsBase - (0.28f * intensityDec) - (intensityInt * 2.8f) - ((intensityDec - 5) * 0.04f) - ((intensityInt - 2) * 0.3f) - ((intensityDec - 5) * 0.04f) - (0.5f * (intensityInt - 12)) + 5 + (intensityDec * 0.36f);
+                    break;
+            }
+
+            return (float)Math.Sqrt((unitCalculation / 2) * unitSize);
         }
 
         public override void CopySettings(DisasterBaseService disaster)
